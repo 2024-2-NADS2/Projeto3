37d851f97cab8e7322c01480d0a022a239c99366
Rafaela Coelho Bastos <159284004+rafaelacoelhob@users.noreply.github.com>
2024-10-28 19:51:16 -0300

    Pagina de contato

---
+++ src/Frontend/Contato react/src/App.js
@@ -0,0 +1,17 @@
+// src/App.js
+import React from 'react';
+import Header from './components/Header';
+import Contato from './components/Contato';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Contato />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
