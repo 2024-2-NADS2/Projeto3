2f3a3d1cfce0039ddae9325b7957202b2bb905f8
Rafaela Coelho Bastos <159284004+rafaelacoelhob@users.noreply.github.com>
2024-10-28 19:48:43 -0300

    Home pagina react

---
+++ src/Frontend/Pagina home react/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Ranking from './components/Ranking';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
