5ff62fbdaa2d1961ebb78e17967f232ae43268c1
Deivid140904 <dg475163@gmail.com>
2024-10-29 16:45:27 -0300

    subindo pagina Termos e condições react

---
+++ src/Frontend/Termos react/src/Components/Condicoes/Condicoes.js
@@ -0,0 +1,79 @@
+import './Texto.css';
+
+const TermsAndConditions = () => {
+  return (
+    <div style={{ padding: '20px', maxWidth: '800px', margin: 'auto', fontFamily: 'Arial' }}>
+      <h1 style={{ color: 'green' }}>TERMOS E CONDIÇÕES GERAIS DO USO DO SITE</h1>
+
+      <h3>1. Aceitação dos Termos</h3>
+      <p>
+        Ao acessar e utilizar nosso site, você declara que tem a idade mínima de 18 anos ou que possui o consentimento de um responsável legal. 
+        Ao utilizar o site, você concorda em respeitar todos os termos e condições aqui descritos.
+      </p>
+
+      <h3>2. Descrição dos Serviços</h3>
+      <p>
+        O site oferece uma plataforma para que beneficiários possam agendar horários para a coleta de alimentos disponibilizados pela ONG. 
+        As informações sobre horários e disponibilidade são fornecidas pela ONG e podem ser alteradas sem aviso prévio.
+      </p>
+
+      <h3>3. Cadastro de Usuário</h3>
+      <p>
+        Para agendar um horário, será necessário que você se registre como usuário. Ao se registrar, você concorda em fornecer 
+        informações precisas, completas e atualizadas. Você é responsável por manter a confidencialidade de suas informações 
+        de login e por todas as atividades que ocorrem sob sua conta.
+      </p>
+
+      <h3>4. Uso do Site</h3>
+      <p>
+        Você concorda em usar o site apenas para fins legais e de acordo com todas as leis aplicáveis. É proibido: 
+        <ul>
+          <li>Utilizar o site para qualquer finalidade fraudulenta ou não autorizada.</li>
+          <li>Interferir na segurança ou no operacionamento do site.</li>
+          <li>Enviar informações falsas ou enganosas.</li>
+        </ul>
+      </p>
+
+      <h3>5. Propriedade Intelectual</h3>
+      <p>
+        Todo o conteúdo do site, incluindo textos, gráficos, logotipos, imagens e software, é de propriedade do Sabor Solidário 
+        ou de seus licenciadores e está protegido por leis de direitos autorais e propriedade intelectual. Você não pode reproduzir, 
+        modificar, distribuir ou utilizar qualquer conteúdo sem nossa autorização prévia.
+      </p>
+
+      <h3>6. Limitação de Responsabilidade</h3>
+      <p>
+        O Sabor Solidário não será responsável por qualquer dano direto, indireto, incidental, especial ou consequencial decorrente 
+        do uso ou da incapacidade de uso do site. As informações fornecidas no site são apenas informativas, e o Sabor Solidário 
+        não garante a precisão, integridade ou atualidade dessas informações.
+      </p>
+
+      <h3>7. Alterações nos Termos</h3>
+      <p>
+        Reservamo-nos o direito de modificar estes Termos de Uso a qualquer momento. As alterações entrarão em vigor assim que 
+        publicadas no site. Recomendamos que você revise periodicamente esta página para estar ciente de quaisquer alterações.
+      </p>
+
+      <h3>8. Links para Outros Sites</h3>
+      <p>
+        Nosso site pode conter links para sites de terceiros. Não temos controle sobre esses sites e não nos responsabilizamos 
+        por qualquer conteúdo ou práticas de privacidade de terceiros. A inclusão de qualquer link não implica endosso ou associação 
+        de nossa parte com esses sites vinculados.
+      </p>
+
+      <h3>9. Legislação Aplicável</h3>
+      <p>
+        Estes Termos de Uso são regidos pelas leis do Brasil, independentemente de conflitos de princípios legais. Você concorda 
+        que se submeterá à jurisdição exclusiva dos tribunais de São Paulo, São Paulo.
+      </p>
+
+      <h3>10. Contato</h3>
+      <p>
+        Se você tiver dúvidas sobre estes Termos de Uso, entre em contato conosco através do email 
+        SaborSolidario@gmail.com ou pelo telefone (11) 00000-7129.
+      </p>
+    </div>
+  );
+};
+
+export default TermsAndConditions;
